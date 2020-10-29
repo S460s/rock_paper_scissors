68b59b43622412b60a5df6b43c5a39cffb3f07f5
S460 <aleksandar.ta6ev@gmail.com>
2020-10-29 11:07:35 +0200

    Everything is done except a nice UI

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
         let playerScore  = 0;
         let computerScore = 0;
         const container = document.querySelector("#display");
-        const buttons = document.querySelectorAll(".btn");
+        const buttons = document.querySelectorAll("#btn");
 
 
         function computerPlay() {
@@ -11,14 +11,12 @@
                 case 1: return "scissors"
                 case 2: return "paper"
                     }
-            }
+                }
 
         function playRound(playerSelection, computerSelection) {
             // Compares playerSelection and computerSelection
             const msg = document.createElement("p")
-            
-            
-            
+        
             if (playerSelection === "rock" && computerSelection ==="paper"){
                 computerScore++            
                 document.getElementById('scoreComputer').innerText =(computerScore)
@@ -53,37 +51,31 @@
             else if (playerSelection === computerSelection) {
                 document.getElementById('result').innerText =("Tie!")
                 }
-        }
-
-
-
+            }
 
 
         function game() {
             buttons.forEach((button1) => {
-                
                 button1.addEventListener("click",() => {
                     let computerSelection = computerPlay();
-                    playRound(button1.id,computerSelection)      
+                    if (button1.id === "btn") {
+                        playRound(button1.value,computerSelection) 
+                    }
                     if (playerScore === 5) {
                         document.getElementById('congrats').innerText = `Congrats! You won! The results is ${playerScore} for you and ${computerScore} for the computer`
                     }
                     else if (computerScore === 5) {
                         document.getElementById('congrats').innerText = `You lost! The results is ${playerScore} for you and ${computerScore} for the computer`
                     }
-                        if (playerScore === 5 || computerScore === 5) {
-                            console.log("test")
-                            let all = document.querySelectorAll("button")
-                            all.forEach((button) => {
-                                console.log(button)
-                                button.classList.remove("btn")
-                                button.removeAttribute("id")
-                            })
-                        }
+                    if (playerScore === 5 || computerScore === 5) {
+                        buttons.forEach((button) => {
+                        button.removeAttribute("id")
+                        })
+                    }
                 })
             })
         }
-
+function playAgain(){
 const playAgain = document.querySelector('#playAgain');
 playAgain.addEventListener('click', () => {
     computerScore = 0;
@@ -92,13 +84,16 @@ playAgain.addEventListener('click', () => {
     document.getElementById("scoreComputer").innerText = computerScore;
     document.getElementById('result').innerText ="Welcome to RPC! Click any of the buttons below to start."
     document.getElementById("congrats").innerText = ""
+    buttons.forEach((button) => {
+        button.setAttribute("id", "btn")
+    })
 
 });
-       
+}
 
 
 game()
-
+playAgain()
 
 
 
